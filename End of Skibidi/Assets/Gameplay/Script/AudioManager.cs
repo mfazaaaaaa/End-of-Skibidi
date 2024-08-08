c7e8b80c3e3814cb0ef5af0d2f6a6fa3ca0b1832
putrafariz <cvrfac@gmail.com>
2024-08-08 09:06:45 +0700

    Akhirnya audio masuk

---
+++ End of Skibidi/Assets/Gameplay/Script/AudioManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("---------- Audio Source ----------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("---------- Audio Clip ----------")]
+    public AudioClip background;
+
+    private static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if (musicSource != null && background != null)
+        {
+            musicSource.clip = background;
+            musicSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning("MusicSource or Background AudioClip is not set!");
+        }
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        if (SFXSource != null && clip != null)
+        {
+            SFXSource.PlayOneShot(clip);
+        }
+        else
+        {
+            Debug.LogWarning("SFXSource or AudioClip is not set!");
+        }
+    }
+}
