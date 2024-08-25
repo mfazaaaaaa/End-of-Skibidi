b9ba03907d8c65b69d7807a888b58c5ac756fbc0
mfazaaaaaa <muhammadfazaaisi@gmail.com>
2024-08-25 19:03:33 +0700

    bintang skibidi

--- End of Skibidi/Assets/Gameplay/Script/GameplayManager.cs
+++ End of Skibidi/Assets/Gameplay/Script/GameplayManager.cs
@@ -30,23 +30,38 @@ public class GameplayManager : MonoBehaviour
     public void CalculateStars()
     {
         float halfEnemies = totalEnemies / 2f;
+        int starCount = 1; // Default 1 bintang
 
         if (defeatedEnemies == totalEnemies)
         {
             // 3 Bintang jika semua musuh dikalahkan
+            starCount = 3;
             ActivateStars(3);
         }
         else if (defeatedEnemies >= halfEnemies)
         {
             // 2 Bintang jika setengah atau lebih musuh dikalahkan
+            starCount = 2;
             ActivateStars(2);
         }
         else
         {
             // 1 Bintang jika hanya menyelesaikan level
+            starCount = 1;
             ActivateStars(1);
         }
+
+        // Simpan jumlah bintang terbaik di PlayerPrefs
+        int currentLevel = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+        int storedStars = PlayerPrefs.GetInt("Level" + currentLevel + "Stars", 0);
+
+        // Simpan hanya jika starCount lebih tinggi dari storedStars
+        if (starCount > storedStars)
+        {
+            PlayerPrefs.SetInt("Level" + currentLevel + "Stars", starCount);
+        }
     }
+
 
     // Fungsi untuk mengaktifkan bintang sesuai jumlah
     private void ActivateStars(int starCount)
--- End of Skibidi/Assets/Gameplay/Script/LevelManager.cs
+++ End of Skibidi/Assets/Gameplay/Script/LevelManager.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class LevelStars
+{
+    public GameObject[] starImages; // Array bintang untuk setiap level
+}
+
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] private Button[] levelButtons; // Array tombol level
+    [SerializeField] private LevelStars[] levelStars; // Array kelas LevelStars untuk setiap level
 
     private void Start()
     {
@@ -16,6 +23,24 @@ public class LevelManager : MonoBehaviour
             if (i + 1 > levelReached)
             {
                 levelButtons[i].interactable = false; // Nonaktifkan level yang belum dibuka
+            }
+
+            // Ambil jumlah bintang yang disimpan untuk level ini
+            int starsEarned = PlayerPrefs.GetInt("Level" + (i + 1) + "Stars", 0);
+
+            // Nonaktifkan semua bintang sebelum menampilkan bintang yang diperoleh
+            foreach (GameObject star in levelStars[i].starImages)
+            {
+                star.SetActive(false);
+            }
+
+            // Tampilkan jumlah bintang yang diperoleh
+            for (int j = 0; j < starsEarned; j++)
+            {
+                if (j < levelStars[i].starImages.Length)
+                {
+                    levelStars[i].starImages[j].SetActive(true); // Aktifkan gambar bintang
+                }
             }
         }
     }
